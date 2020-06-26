9c543ed9f2e39a960cfb36d5115e65fed1d7bd3a
Yasmin Ahuja <yahuja21@hb.edu>
2020-06-25 20:44:33 -0400

    most recent

---
+++ FinalProject/AllyshipDictionaryViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AllyshipDictionaryViewController.swift
+//  FinalProject
+//
+//  Created by Yasmin Ahuja on 6/25/20.
+//  Copyright © 2020 Yasmin Ahuja. All rights reserved.
+//
+
+import UIKit
+
+class AllyshipDictionaryViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //self.navigationItem.hidesBackButton = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
